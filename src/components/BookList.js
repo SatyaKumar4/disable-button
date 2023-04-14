b2f37415868c66346c56ef5f148b003d1e2bd3a3
SatyaKumar4 <satyakumars@outlook.com>
2023-04-14 11:11:38 +0530

    disable-cart button

---
+++ src/components/BookList.js
@@ -0,0 +1,70 @@
+import {useState} from 'react'
+
+const cover = "https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL600_SR600,400_.jpg"
+const title = "Atomic Habits";
+const Author = "James Clear";
+const price =50;
+
+const BookObject = [
+    {
+        cover: "https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL600_SR600,400_.jpg",
+        title: "Atomic Habits",
+        author: "James Clear"
+    },
+    {
+        cover: 'https://images-eu.ssl-images-amazon.com/images/I/71g2ednj0JL._AC_UL600_SR600,400_.jpg',
+        title: "The Psychology of money",
+        author: "Morgan Housel"
+    },
+    {
+        cover: 'https://images-eu.ssl-images-amazon.com/images/I/81N4EM2zVSL._AC_UL600_SR600,400_.jpg',
+        title: "The hidden hindu",
+        author: "Akshat Gupta"
+    }
+
+];
+
+
+
+const BookList = () => {
+    return (
+        <section className="BookList">
+            <Book author={Author} title={title} cover={cover} />
+            <Book author={Author} title={title} cover={cover} />
+
+        </section>
+    )
+}
+
+// const BookDetails = BookObject.map(()=>)
+
+
+export const Book = ({ cover, title, author }) => {
+    const [addedToCart, setAddedToCart] = useState(false);
+    const [cartCount, setCartCount] = useState(0);
+  
+    const handleAddToCart = () => {
+      setAddedToCart(true);
+      setCartCount(cartCount + 1);
+    }
+
+    return (
+
+        <article className="Book">
+            <img src={cover} alt='Atomic Habits'></img>
+            <h2>{title}</h2>
+            <h4>{author}</h4>
+            <p>${price}</p>
+            {!addedToCart && <button onClick={handleAddToCart} type="button" class="btn btn-dark">Add to cart</button>}
+            {addedToCart && <button disabled type="button" class="btn btn-dark">Added to cart</button>}
+            <p>{cartCount}</p>
+        </article>
+    )
+}
+
+
+
+
+
+
+export default BookList;
---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+const NavBar = ()=>{
+    return( 
+        <div className="navbar">
+           <ul>
+            <li>home</li>
+            <li>about</li>
+            <li>shop</li>
+            <li>contact</li>
+           </ul>
+           <i class="fa-solid fa-cart-shopping"></i>
+        </div>
+    )
+}
+
+export default NavBar;
